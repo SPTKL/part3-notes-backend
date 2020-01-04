254c1031e98131a1a70b35ed10575f86ed93895c
sptkl <caobaiyue@gmail.com>
2020-01-04 13:43:22 -0500

    udpate

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const express = require('express')
 const app = express()
-
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
-let notes = [
+let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
+    name: "A",
+    number: "2346512834512834",
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
+    name: "b",
+    number: "38465234",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
+    name: "C",
+    number: "23423412345",
   }
 ]
 
@@ -30,56 +26,34 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+app.get('/api/persons', (req, res) => {
+  res.send(persons)
 })
 
-app.get('/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  res.send(person)
 })
 
-app.get('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id != id)
+  res.status(204).end()
 })
 
-app.delete('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+  person.id = Math.floor(Math.random() * 100)
+  console.log(person)
+  res.send(person)
 })
 
-const PORT = 3001
+app.get('/info', (req, res) => {
+  res.send(`<p>Phone book has ${persons.length} people</p><p>${Date()}</p>`)
+})
+
+const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
